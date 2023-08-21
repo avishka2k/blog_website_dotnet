ff4316421bcf4e6e109f24f159afca08f51c8037
avishka2k <avishka2k@gmail.com>
2023-08-22 00:32:07 +0530

    initail project

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="blogsite_asp.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(blogsite_asp.Startup))]
+namespace blogsite_asp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
